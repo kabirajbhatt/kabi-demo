d72c497378c49753c52e2420e757d3e7a572a3ac
Kabi Raj Bhatt <kavirajbhatt62@gmail.com>
2026-02-04 15:54:23 +0545

    Add files via upload

---
+++ bca.java
@@ -0,0 +1,10 @@
+public class bca{
+  public static void main(String[] args) {
+    StringBuffer obj = new stringBuffer(str:"Hello");
+    obj.append(str:"HI");
+    obj.insert(0,"Hey");
+    obj.delete(0,3);
+    obj.reverse();
+    obj
+  }
+}
